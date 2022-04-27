87763f75eff28ad26566ad29bf31d1224aaae475
Areeb-M <am0247@uah.edu>
2022-04-27 16:21:12 -0500

    Initial version of APRS Hook.

--- internal/aprs/aprs_hook.go
+++ internal/aprs/aprs_hook.go
@@ -2,39 +2,69 @@ package aprs
 
 import (
 	"net"
+	"strconv"
 	"strings"
 
+	"github.com/APRS-Mission-Manager/aprs-interface/internal/config"
 	"github.com/rs/zerolog/log"
 )
 
-func InitializeHook() {
-	log.Info().Msg("Initializing APRS Hook.")
+type Hook struct {
+	conn      net.Conn
+	appConfig config.Config
+}
 
-	conn, err := net.Dial("tcp", "noam.aprs2.net:14580")
-	checkErr(err)
+func CreateHook(appConfig config.Config) Hook {
+	hook := Hook{appConfig: appConfig}
 
-	defer conn.Close()
+	return hook
+}
 
-	loginMsg := "user kn4cdd pass -1 filter r/33/-97/200\r\n"
+func (hook Hook) Subscribe() {
+	log.Info().Msg("[APRS Hook] Initializing APRS Hook.")
 
-	_, err = conn.Write([]byte(loginMsg))
-	checkErr(err)
+	serverAddress := hook.appConfig.APRS.Server + ":" + strconv.Itoa(hook.appConfig.APRS.Port)
+	conn, err := net.Dial("tcp", serverAddress)
+	if err != nil {
+		log.Fatal().AnErr("error", err).Msg("[APRS Hook] Failed while connecting to " + serverAddress)
+	}
+	hook.conn = conn
 
-	replyBuffer := make([]byte, 128)
-	replyBuffer[127] = 'A'
-
+	hook.login()
+	replyBuffer := make([]byte, 256)
 	for {
-		_, err = conn.Read(replyBuffer)
-		checkErr(err)
+		_, err := conn.Read(replyBuffer)
+		if err != nil {
+			log.Fatal().AnErr("error", err).Msg("[APRS Hook] Failed while receiving message from server.")
+		}
 
-		packet := strings.Split(string(replyBuffer), "\r\n")[0]
+		message := strings.Split(string(replyBuffer), "\r\n")[0]
 
-		log.Info().Msg(packet)
+		if message[0] == '#' {
+			hook.handleServerUpdate(message)
+		} else {
+			hook.handleAPRSPacket(message)
+		}
 	}
 }
 
-func checkErr(err error) {
-	if err != nil {
-		log.Fatal().AnErr("error", err)
+func (hook Hook) login() {
+	filterMsg := " filter"
+	for i := range hook.appConfig.APRS.CallsignPatterns {
+		filterMsg = filterMsg + " " + hook.appConfig.APRS.CallsignPatterns[i]
 	}
+	loginMsg := "user " + hook.appConfig.APRS.Username + " pass " + hook.appConfig.APRS.Password + filterMsg + "\r\n"
+
+	_, err := hook.conn.Write([]byte(loginMsg))
+	if err != nil {
+		log.Fatal().AnErr("error", err).Msg("[APRS Hook] Failed while sending login message to server.")
+	}
+}
+
+func (hook Hook) handleAPRSPacket(packet string) {
+	log.Debug().Str("packet", packet).Msg("[APRS Hook]")
+}
+
+func (hook Hook) handleServerUpdate(update string) {
+	log.Debug().Str("update", update).Msg("[APRS Hook]")
 }
