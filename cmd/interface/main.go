b0c4d9069023cdc00c7db907dea9d83ec1384f15
Areeb-M <am0247@uah.edu>
2022-04-27 09:06:20 -0500

    Replaced go log library with zerolog in file.

--- cmd/interface/main.go
+++ cmd/interface/main.go
@@ -2,16 +2,24 @@ package main
 
 import (
 	"io"
-	"log"
 	"net/http"
+
+	"github.com/APRS-Mission-Manager/aprs-interface/internal/aprs"
+	"github.com/rs/zerolog/log"
 )
 
 func main() {
+	go aprs.InitializeHook()
+
 	helloHandler := func(w http.ResponseWriter, req *http.Request) {
 		io.WriteString(w, "Hello, world!\n")
 	}
 
 	http.HandleFunc("/hello", helloHandler)
-	log.Println("Listening for requests at http://localhost:8080/hello")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Info().Msg("Listening for requests at http://localhost:8080/hello")
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal().AnErr("error", err)
+	}
 }
